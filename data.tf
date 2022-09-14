1c0fff13c279a6e7a47f129030eaaf2e6101ce80
RalphBrynard <ralph@thebrynards.com>
2022-09-14 17:16:28 +0000

    feature: iam role docs: tfdocs test: deploy

--- data.tf
+++ data.tf
@@ -11,6 +11,19 @@ data "http" "my_public_ip" {
   }
 }
 
+data "aws_iam_policy_document" "ec2_iam_policy" {
+  statement {
+    actions = [
+      "secretsmanager:GetSecretValue",
+      "secretsmanager:DescribeSecret"
+    ]
+    resources = [
+      aws_secretsmanager_secret.console_password.arn,
+      aws_secretsmanager_secret.config_backup_password.arn
+    ]
+  }
+}
+
 data "aws_iam_policy_document" "trust_relationship" {
   statement {
     actions = ["sts:AssumeRole"]
