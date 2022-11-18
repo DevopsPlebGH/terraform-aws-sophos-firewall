bcb53fc1c96fbfa44c2c81b5e948f7c8b4a0c2e8
Dev0psPleb <ralph@thebrynards.com>
2022-11-18 17:26:43 +0000

    Pushing commit to branch to test GH Actions workflow

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,8 +1,17 @@
+locals {
+  aws_region        = data.aws_region.current.name
+  availability_zone = element(random_shuffle.az.result, 0)
+}
+
+resource "random_shuffle" "az" {
+  input        = try(data.aws_availability_zones.available.names[*])
+  result_count = 1
+}
 module "complete" {
   source                 = "../.."
   accept_eula            = "yes"
-  availability_zone      = "us-west-2c"
-  aws_region             = "us-west-2"
+  availability_zone      = local.availability_zone
+  aws_region             = local.aws_region
   create_vpc             = true
   config_backup_password = "Password!123"
   console_password       = "Password!123"
